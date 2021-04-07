b5fcf12c7fcf788441bad36e2d2cc4306c25529b
vedantsingh1995 <45996469+vedantsingh1995@users.noreply.github.com>
2021-04-07 18:12:46 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,5 +8,5 @@ def hello():
     #d["subscription_id"] = subs_id
     #print (d)
     resp = flask.Response("Foo bar baz")
-    resp.headers['Ocp-Apim-Subscription-Key'] = '3947aa67fd0c434a996c597775c04176'   
+    resp.headers['Ocp-Apim-Subscription-Key'] = ''   
     return resp
