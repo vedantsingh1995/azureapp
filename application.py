ba1548f9a5ded31db033c11ebaf703f5698dc9ef
vedantsingh1995 <45996469+vedantsingh1995@users.noreply.github.com>
2021-04-07 18:11:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,8 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    d = {}
-    subs_id = "xxxx"
-    d["subscription_id"] = subs_id
-    print (d)	
-    return "<h1>Hello World!</h1>"
+    #d = {}
+    #subs_id = "xxxx"
+    #d["subscription_id"] = subs_id
+    #print (d)
+    resp = flask.Response("Foo bar baz")
+    resp.headers['Ocp-Apim-Subscription-Key'] = '3947aa67fd0c434a996c597775c04176'   
+    return resp
