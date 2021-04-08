701c106694c98e7c2c4ae7fea328028fefba9df9
vedantsingh1995 <45996469+vedantsingh1995@users.noreply.github.com>
2021-04-08 18:15:18 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,13 +4,15 @@ from flask import Response
 app = Flask(__name__)
 
 @app.route("/")
+#@app.after_request
 def hello():
     #d = {}
     #subs_id = "xxxx"
     #d["subscription_id"] = subs_id
     #print (d)
-    resp = Flask.Response("Foo bar baz")
+    #resp = Flask.Response("Foo bar baz")
     print (os.environ["test"])
-    resp.headers['Ocp-Apim-Subscription-Key'] = 'sjhdsdjs'
-    return resp
+    return Response(headers={'Access-Control-Allow-Origin':'*'})
+    #resp.headers['Ocp-Apim-Subscription-Key'] = 'sjhdsdjs'
+    #return resp
     #return "test"
