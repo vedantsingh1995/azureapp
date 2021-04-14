ea981e428a555984d4c71daed68bed3dcf8ce585
vedantsingh1995 <45996469+vedantsingh1995@users.noreply.github.com>
2021-04-14 16:52:15 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,18 +1,16 @@
 from flask import Flask
 import os
 from flask import Response
+from flask import request
+import json
 app = Flask(__name__)
 
 @app.route("/",methods = ['POST', 'GET'])
 #@app.after_request
 def hello():
-    #d = {}
-    #subs_id = "xxxx"
-    #d["subscription_id"] = subs_id
-    #print (d)
-    #resp = Flask.Response("Foo bar baz")
+    req_data = request.get_json()
     head = (os.environ["test"])
-    return Response(headers={'Ocp-Apim-Subscription-Key': head})
-    #resp.headers['Ocp-Apim-Subscription-Key'] = 'sjhdsdjs'
-    #return resp
-    #return "test"
+    response = Response()
+    response.headers["Ocp-Apim-Subscription-Key"] = head
+    response.data = json.dumps(req_data)
+    return response
